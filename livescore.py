171f7409d3570ba700a81d125b1518befbf72c01
Ezer Neupane Rames <ezerames@gmail.com>
2015-12-03 00:43:13 +0545

    alert notify

--- livescore.py
+++ livescore.py
@@ -6,7 +6,8 @@ import requests,re
 import colors as c
 import subprocess
 import time,os
-
+score1 = [0]*110
+score2 = [0]*110
 def sendAlert(message):
     subprocess.Popen(['notify-send',message])
     return
@@ -29,56 +30,55 @@ def _process(rows,flag):
     if flag == 'scores':
         return [re.split('   |-', _livescores[i]) for i in range(len(_livescores))]
     else:
-        return False
-
+        return [re.split('   ', _scoreTable[i]) for i in range(len(_scoreTable))]
+        #return _scoreTable
 
 def print_scores(x):
-    
+                
     COLOR2 = c.GREEN
     COLOR3 = c.GREEN
-    scores = 'BPL SCORES'
-    
-    print(c.BLUE+'------------------------------------------------------------')
+    scores = 'BPL SCORES'        
+    print(c.BLUE+'\n------------------------------------------------------------')
     print('\t\t\t'+c.GREEN+scores)
     print(c.BLUE+'------------------------------------------------------------'+c.END)
+
     for i in range(len(x)-1):
-    	piss = [p.strip() for p in x[i]]
-    	
-    	if int(piss[2]) > int(piss[3]):
+        piss = [p.strip() for p in x[i]]                
+        if int(piss[2]) == int(score1[i]) or int(piss[3]) == int(score2[i]) :
+            sendAlert(piss[0]+'   '+piss[1]+' '+piss[2]+' - '+piss[3]+' '+piss[4])
+            
+        if int(piss[2]) > int(piss[3]):
             COLOR2 = c.ORANGE
             COLOR3 = c.RED
-    	elif int(piss[2]) < int(piss[3]):
+        elif int(piss[2]) < int(piss[3]):
             COLOR3 = c.ORANGE
             COLOR2 = c.RED
-	else:
-	    COLOR2 = c.CYAN
-	    COLOR3 = c.CYAN
-        
+        else:
+            COLOR2 = c.CYAN
+            COLOR3 = c.CYAN
+        score1.append(piss[2])
+        score2.append(piss[3])
         print(piss[0]+'\t'+COLOR2+''.join(piss[1].ljust(16))+'\t'+piss[2]+c.END+' - '+COLOR3+piss[3]+'\t'+piss[4]+c.END)
-    
+        
     print(c.BLUE+'------------------------------------------------------------')
-
+    
 
 def main():
     
     url = 'http://www.livescore.com/soccer/england/premier-league/'
-    
+
     while True:
         try:
-            print('...fetching scores from livescore...')
+            print('Fetching scores from livescore ')
             rows = get_livescore(url)
-	    os.system('clear')
+            time.sleep(5)
+            os.system('clear')
             print_scores(_process(rows,'scores'))
             
-            time.sleep(10)
-
         except KeyboardInterrupt:
             break
 
         
-
-
-
 
 if __name__ == '__main__':
     main()
