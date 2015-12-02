829f54ae73888f82b748440d4ed8f4937e74a909
Ezer Neupane Rames <ezerames@gmail.com>
2015-12-02 22:54:02 +0545

    last for today

--- livescore.py
+++ livescore.py
@@ -4,6 +4,13 @@
 from bs4 import BeautifulSoup
 import requests,re
 import colors as c
+import subprocess
+import time,os
+
+def sendAlert(message):
+    subprocess.Popen(['notify-send',message])
+    return
+
 
 def get_livescore(url):
     r = requests.get(url)
@@ -26,35 +33,49 @@ def _process(rows,flag):
 
 
 def print_scores(x):
-    scores = 'BPL SCORES'
+    
     COLOR2 = c.GREEN
     COLOR3 = c.GREEN
+    scores = 'BPL SCORES'
+    
     print(c.BLUE+'------------------------------------------------------------')
     print('\t\t\t'+c.GREEN+scores)
     print(c.BLUE+'------------------------------------------------------------'+c.END)
-    
     for i in range(len(x)-1):
     	piss = [p.strip() for p in x[i]]
     	
     	if int(piss[2]) > int(piss[3]):
-            COLOR2 = c.RED
-            COLOR3 = c.CYAN
-    	elif int(piss[2]) < int(piss[3]):
+            COLOR2 = c.ORANGE
             COLOR3 = c.RED
-            COLOR2 = c.CYAN
+    	elif int(piss[2]) < int(piss[3]):
+            COLOR3 = c.ORANGE
+            COLOR2 = c.RED
 	else:
-	    COLOR2 = c.GREEN
-	    COLOR3 = c.GREEN
+	    COLOR2 = c.CYAN
+	    COLOR3 = c.CYAN
         
         print(piss[0]+'\t'+COLOR2+''.join(piss[1].ljust(16))+'\t'+piss[2]+c.END+' - '+COLOR3+piss[3]+'\t'+piss[4]+c.END)
-        
+    
     print(c.BLUE+'------------------------------------------------------------')
 
 
 def main():
+    
     url = 'http://www.livescore.com/soccer/england/premier-league/'
-    rows = get_livescore(url)
-    print_scores(_process(rows,'scores'))
+    
+    while True:
+        try:
+            print('...fetching scores from livescore...')
+            rows = get_livescore(url)
+	    os.system('clear')
+            print_scores(_process(rows,'scores'))
+            
+            time.sleep(10)
+
+        except KeyboardInterrupt:
+            break
+
+        
 
 
 
