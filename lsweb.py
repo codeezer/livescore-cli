6590ccb6b9f48cef8b3f930862909cecd044560d
Ezer Neupane Rames <ezerames@gmail.com>
2016-01-01 06:59:46 +0545

    date scraps

--- lsweb.py
+++ lsweb.py
@@ -10,9 +10,17 @@ def check_ping(hostname):
     else:
         return False
 
+
 #main webscrapping code which take the url to scrap and returns the rows of data
 def get_livescore(url,scrapping_class):
     r = requests.get(url)
     soup = BeautifulSoup(r.text,'html.parser')
     _rows = soup.find_all(class_=scrapping_class)
     return _rows
+
+def get_dates(url,date_class):
+    raw = get_livescore(url,date_class)
+    dates = '\n'.join(map(lambda r: r.text, raw))
+    array_dates = re.split('\n',dates)
+    return array_dates
+
