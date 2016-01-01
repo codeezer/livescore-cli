7a332e95881e55a0863c63352194e48b80b3976b
anujgtm1 <anuj_gtm1@hotmail.com>
2016-01-01 21:18:00 +0545

    Added the time comparison function. Also did some changes in the main flow of code

--- livescore.py
+++ livescore.py
@@ -9,11 +9,15 @@ import URL #Contains the urls to fetch data
 import cli #Contains our command line interface code
 
 def main():
+    bTable = bool(cli.args.table)
+    bScore = bool(cli.args.score)
+    bScorers = bool(cli.args.scorers)
+    if not bTable or not bScore or not bScorers:
+        bScore = True
     while True:
         try:   
             os.system('clear')
             print(' ... Fetching scores from http://www.livescore.com ... ')
- 
             for k in cli.args.League:
             	
                 #Code to fetch data from URL[k]
@@ -23,26 +27,30 @@ def main():
                 if lsweb.check_ping(pingTest) == True:
                     rows = lsweb.get_livescore(url,'row-gray')
     
-                    if cli.args.table:
+                    if bTable:
                         print("Displaying Table for {}".format(URL.URL[k][0]))
                         lsprint.table(lsprocess.pretty_array(rows,'table'),k)
                 
 #                   if cli.args.fixtures:
 #                       print("Displaying Fixtures for {}".format(URL.URL[k][0]))
                 
-                    if cli.args.score:
+                    if bScore:
                         print("Displaying Scores for {}".format(URL.URL[k][0]))
                         lsprint.scores(lsprocess.pretty_array(rows,'scores'),k)
     
-                    if cli.args.scorers:
+                    if bScorers:
                         print("Displaying Top Scorers for {}".format(URL.URL[k][0]))
                         scorer_rows = lsweb.get_livescore(url_scorer,'competition-top-scorers-list')
                         lsprint.scorers(lsprocess.pretty_array(scorer_rows,'scorers'),k)
-    
+#            bTable = False
+#            bScorers = False
                 else:
                     print("Check Your Internet Connection , It looks like you're out of internet.")
                 time.sleep(3)
-        
+            bTable = False
+            bScorers = False 
+            if not bool(bScore):
+                break
             time.sleep(25)
             
         except KeyboardInterrupt:
--- tt.py
+++ tt.py
@@ -9,6 +9,16 @@ NOTE: any string not in the format "digits:digits" will be returned as is
 USAGE: 
 >>>convert(['19:45','18:15','5:45','512','FT'])
 ['01:00','00:00','11:30','512','FT']
+
+isgreat(time1,time2) takes two time strings such as "4:30" and "12"15"
+and returns if the first time is greater than the second.
+if time1>time2: return 1
+if time2>time1: return -1
+if time1==time2: return 0
+NOTE: the function stalls if not in the above format
+USAGE:
+>>>isgreat("3:00","4:15")
+-1
 '''
 
 if time.daylight:
@@ -44,3 +54,20 @@ def _fix(y):
 def convert(times):
 	times = list(map(_conv, times))
 	return times
+
+def isgreat(time1,time2):
+	t1 = list(map(int,time1.split(':')))
+	t2 = list(map(int,time2.split(':')))
+	if t1[0] > t2[0]:
+		return 1
+	elif t2[0] > t1[0]:
+		return -1
+	else:
+		if t1[1] > t2[1]:
+			return 1
+		elif t2[1] > t1[1]:
+			return -1
+		else:
+			return 0
+
+
