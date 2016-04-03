61bc379fc1f8432e3b22d0633d47fa87197367ad
Anuj Gautam <anuj_gtm1@hotmail.com>
2016-04-03 10:18:10 +0545

    Fixed pep8 standards for some files

--- livescore.py
+++ livescore.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import requests, re, os, argparse
+import os
 from lib import lscolors as c
 from lib import cli
 from lib import URL
@@ -10,7 +10,8 @@ from lib import lsprint
 from lib import lsweb
 from lib import lsnews
 
-import time,json
+import time
+
 
 def main():
     bTable = bool(cli.args.table)
@@ -20,49 +21,47 @@ def main():
 
     if not bTable and not bScore and not bScorers and not bNews:
         bScore = True
-    
+
     while True:
-        try:   
+        try:
             os.system('clear')
             for k in cli.args.League:
-            	
-                #Code to fetch data from URL[k]
-                url = URL.URL[k][1] 
+                # Code to fetch data from URL[k]
                 pingTest = 'www.livescore.com'
                 print(' ... Fetching scores from '+pingTest+' ... ')
-                if lsweb.is_connected(pingTest) == True:
+                if lsweb.is_connected(pingTest) is True:
 
                     if bTable:
                         print("Displaying Table for {}".format(URL.URL[k][0]))
-                        lsprint.table(lsweb.get_table(URL.URL[k][1]),k)
-                
+                        lsprint.table(lsweb.get_table(URL.URL[k][1]), k)
+
                     if bNews:
                         lsnews.print_news(lsnews.get_news())
 
                     if bScore:
                         print("Displaying Scores for {}".format(URL.URL[k][0]))
-                        lsprint.scores(lsweb.get_score(URL.URL[k][1]),k)
+                        lsprint.scores(lsweb.get_score(URL.URL[k][1]), k)
 
                     if bScorers:
-                        print("Displaying Top Scorers for {}".format(URL.URL[k][0]))
+                        print("Displaying Top Scorers for"
+                              " {}".format(URL.URL[k][0]))
                         print('Working on it')
-                
+
                 else:
-                    print(c.RED+"Check Your Internet Connection , It looks like you're out of internet."+c.END)
-                
+                    print(c.RED+"Check Your Internet Connection ,"
+                          " It looks like you're out of internet."+c.END)
+
                 time.sleep(3)
-            
+
             bTable = False
-            bScorers = False 
+            bScorers = False
             bNews = False
             if not bool(bScore):
                 break
             time.sleep(25)
-            
+
         except KeyboardInterrupt:
             break
-    
-    
-           
+
 if __name__ == '__main__':
     main()
