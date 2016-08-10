ce0e3b6c938639d7d954895a54a4bde99a44ae0d
Anuj Gautam <anuj_gtm1@hotmail.com>
2016-08-10 19:45:08 +0545

    Changed the urls to old site

--- lib/URL.py
+++ lib/URL.py
@@ -1,26 +1,26 @@
 URL = {
        'bpl': ["Barclay's Premier League",
-           'http://www.livescore.com/soccer/england/premier-league/',
+           'http://www.livescores.com/soccer/england/premier-league/',
            'http://www.bbc.com/sport/football/premier-league/top-scorers'],
 
        'laliga': ["La Liga BBVA",
-           'http://www.livescore.com/soccer/spain/primera-division/',
+           'http://www.livescores.com/soccer/spain/primera-division/',
            'http://www.bbc.com/sport/football/spanish-la-liga/top-scorers'],
 
        'bundesliga': ["German Bundesliga",
-           'http://www.livescore.com/soccer/germany/bundesliga/',
+           'http://www.livescores.com/soccer/germany/bundesliga/',
            'http://www.bbc.co.uk/sport/football/german-bundesliga/top-scorers'],
 
        'seriea': ["Italian Serie A",
-           'http://www.livescore.com/soccer/italy/serie-a/',
+           'http://www.livescores.com/soccer/italy/serie-a/',
            'http://www.bbc.co.uk/sport/football/italian-serie-a/top-scorers'],
 
        'ligue1': ["French Lige 1",
-           'http://www.livescore.com/soccer/france/ligue-1/',
+           'http://www.livescores.com/soccer/france/ligue-1/',
            'http://www.bbc.co.uk/sport/football/french-ligue-one/top-scorers'],
 
        'portugal': ["Portugese Primera Liga",
-           'http://www.livescore.com/soccer/portugal/liga-sagres/',
+           'http://www.livescores.com/soccer/portugal/liga-sagres/',
            'http://www.bbc.com/sport/football/portuguese-primeira-liga/top-scorers']
        }
 
