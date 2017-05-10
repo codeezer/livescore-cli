1bb6e074fceec8ab130e29a11cfe0d4644c02990
Ramesh Neupane <ezerames@gmail.com>
2017-05-10 11:40:34 +0545

    bug fixes2

--- lib/lsprint.py
+++ lib/lsprint.py
@@ -19,10 +19,7 @@ def sendAlert(message,title=''):
     #bash command to send notification
     bash_command = """osascript -e 'display notification "{}" with title "{}"'""".format(message, title)
     bash_command = 'notify-send -i '+icon_path+' "'+title+'" "'+message+'"'
-    try:
-        os.system(bash_command)
-    except:
-        pass
+    os.system(bash_command+' > /dev/null 2>&1')
     return
 
 #global variable to temporarily store the score of home and away team to compare for notification
